6f35a6ae523fc56969af53d8c2c9d1ba21a70fcc
jungwonyu <sunsetinmygarden@gmail.com>
2022-11-07 22:44:14 +0900

    feat: add create function

--- src/fbase.js
+++ src/fbase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -16,3 +17,4 @@ firebase.initializeApp(firebaseConfig);
 
 export const firebaseInstance = firebase;
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useState } from 'react';
+import { dbService } from '../fbase';
 
-const Home = () => (
-  <>
-    <span>Home</span>
-  </>
-);
+const Home = () => {
+  const [jweet, setJweet] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection('jweets').add({
+      text: jweet,
+      createdAt: Date.now(),
+    });
+    setJweet('');
+  };
+
+  const onChange = (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setJweet(value);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={jweet}
+        onChange={onChange}
+        type='text'
+        placeholder="What's on your mind?"
+        maxLength={120}
+      />
+      <input type='submit' value='Jweet' />
+    </form>
+  );
+};
 
 export default Home;
